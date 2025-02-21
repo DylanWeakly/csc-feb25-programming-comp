c8ae96ec630e5168b8ad853611655c03890a3344
Ethan Reed <jefmacho99@gmail.com>
2025-02-21 16:42:58 -0600

    easy-3.py

---
+++ Ethan-R/easy-3.py
@@ -0,0 +1,15 @@
+"""
+3. Cumulative Sum List (10 pts)
+Write a function that returns a list where each element is the cumulative sum of the elements before it.
+Example: [1, 2, 3] → [1, 3, 6]
+"""
+
+def cumulative_sum(l):
+    l2 = []
+    s = 0
+    for i in l:
+        s += i
+        l2.append(s)
+    return l2
+
+print(cumulative_sum([1, 2, 3, 4, 5, 6]))
