f56c530ae664125635d41f0e9415ae08b2298d2d
Ethan Reed <jefmacho99@gmail.com>
2025-02-21 16:17:52 -0600

    easy-1.py

---
+++ Ethan-R/easy-2.py
@@ -0,0 +1,10 @@
+"""
+2. List Alternator (10 pts)
+Write a function that merges two lists by alternating their elements.
+Example: [1, 2, 3] and [4, 5, 6] → [1, 4, 2, 5, 3, 6]
+"""
+
+def alternator(l1, l2):
+    for i in range()
+
+
