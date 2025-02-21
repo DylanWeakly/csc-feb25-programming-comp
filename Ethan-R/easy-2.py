621be5cb99648f6d9c714d0669b4142e8d2183b6
Ethan Reed <jefmacho99@gmail.com>
2025-02-21 16:54:11 -0600

    easy-2 fixed, friccccing Git

--- Ethan-R/easy-2.py
+++ Ethan-R/easy-2.py
@@ -5,6 +5,16 @@ Example: [1, 2, 3] and [4, 5, 6] → [1, 4, 2, 5, 3, 6]
 """
 
 def alternator(l1, l2):
-    for i in range()
+    if len(l1)>len(l2):
+        length = len(l1)
+    else:
+        length = len(l2)
+    l3 = []
+    for i in range(length):
+        l3.append(l1[i])
+        l3.append(l2[i])
+    return l3
 
-
+l1 = [1, 2, 3]
+l2 = [4, 5, 6]
+print(alternator(l1, l2))
